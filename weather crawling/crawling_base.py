d411f086bc2f057817c287b9c92d4636b8baeca3
Choi hyunsu <87425320+0InTure0@users.noreply.github.com>
2022-06-29 21:56:35 +0900

    온도 추가

--- weather crawling/crawling_base.py
+++ weather crawling/crawling_base.py
@@ -1,4 +1,4 @@
-#https://cosyp.tistory.com/240
+import string
 import urllib
 from bs4 import BeautifulSoup
 import urllib.request
@@ -23,7 +23,7 @@ soup = BeautifulSoup(webpage, 'html.parser')
 cl_weather = soup.find_all('span','blind')
 cl_time = soup.find_all('dt','time')
 cl_this_weather = soup.find('span','weather')
-
+cl_this_temp = soup.find('div','temperature_text')
 
 cl_this_weather_txt = cl_this_weather.text
 
@@ -47,13 +47,27 @@ def crawling_10h_snow():
             print(this_time,"부터 눈이 올 예정입니다 외출시 우산을 챙겨주세요")
             break
 
-print("현재 날씨는",cl_this_weather_txt)
-if(cl_this_weather_txt == "비"):
-    print("비가 오고 있습니다. 외출시 우산을 챙겨주세요")
-elif(cl_this_weather_txt == "흐리고 가끔 비"):
-    print("흐린 날씨에 가끔 비가 올 것입니다. 외출시 우산을 챙겨주세요")
-elif(cl_this_weather_txt == "눈"):
-    print("눈이 오고 있습니다. 외출시 우산을 챙겨주세요")
-else:
-    crawling_10h_rain()
-    crawling_10h_snow()
+def cl_Weather():
+    if(cl_this_weather_txt == "비"):
+        print("비가 오고 있습니다. 외출시 우산을 챙겨주세요")
+    elif(cl_this_weather_txt == "흐리고 가끔 비"):
+        print("흐린 날씨에 가끔 비가 올 것입니다. 외출시 우산을 챙겨주세요")
+    elif(cl_this_weather_txt == "눈"):
+        print("눈이 오고 있습니다. 외출시 우산을 챙겨주세요")
+    else:
+        crawling_10h_rain()
+        crawling_10h_snow()
+
+def cl_Temp():
+    temp_list = list(cl_this_temp.text)
+    string_temp = "".join(temp_list[6:10])
+    float_temp = float(string_temp)
+    print(", 온도는", string_temp,"도 입니다.")
+    if(float_temp >= 28):
+        print("오늘은 매우 덥습니다, 더위 조심하세요")
+    if(float_temp <= 5):
+        print("오늘은 매우 춥습니다, 감기 조심하세요")
+
+print("현재 날씨는",cl_this_weather_txt, end='')
+cl_Temp()
+cl_Weather()

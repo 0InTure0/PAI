970c1feb8870a2039040e55a26a50a03a98e410c
Choi hyunsu <87425320+0InTure0@users.noreply.github.com>
2022-07-14 21:15:15 -0400

    Update stt_test.py

--- STT_TTS/stt_test.py
+++ STT_TTS/stt_test.py
@@ -1,25 +1,11 @@
-#-*- coding:utf-8 -*-
-import urllib3
-import json
-import base64
-openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition"
-accessKey = "87e83362-6183-4a71-930f-5ce4515ea20a"
-audioFilePath = "hello.wav"
-languageCode = "korean"
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something! : ")
+    audio = r.listen(source)
 
-file = open(audioFilePath, "rb")
-audioContents = base64.b64encode(file.read()).decode("utf8")
-file.close()
+with open("audio_file2.wav","wb") as file:
+  file.write(audio.get_wav_data())
 
-requestJson = {"access_key": accessKey, "argument": {"language_code": languageCode, "audio": audioContents}}
-http = urllib3.PoolManager()
-response = http.request("POST", openApiURL, headers={"Content-Type": "application/json; charset=UTF-8"}, body=json.dumps(requestJson))
-
-#print("[responseCode] " + str(response.status))
-#print("[responBody]")
-print((str(response.data,"utf-8")))
-#녹음된 음성만 문자열로 바꿔주는 기능
-list_data = list(str(response.data,"utf-8"))
-data_length = len(list_data)
-a = "".join(list_data[43:data_length-3])
-print(a)
+stt_text = r.recognize_google(audio, language='ko-KR')
+print(stt_text)
